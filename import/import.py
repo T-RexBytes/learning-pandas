da464bf2e8144caafe82ca8da7b54d4d8b2621a7
T-RexBytes <azurebliss12@gmail.com>
2026-05-06 23:08:56 +0530

    add import methods

---
+++ import/import.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+#importing csv file
+# pokemon = pd.read_csv("D:\\learn-pandas\\import\\pokemon.csv")
+# pokemon = pd.read_csv("pokemon.csv")
+
+
+#importing json file
+# pokemon = pd.read_json("pokemon.json")
+pokemon = pd.read_json("D:\\learn-pandas\\import\\pokemon.json")
+
+
+print(pokemon)
